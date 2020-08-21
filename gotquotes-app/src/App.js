527659fa020882ed9382b325c814be1df2d66669
Sachin Ramteke <sachin06jd@gmail.com>
2020-08-21 16:57:40 -0500

    api call working

--- gotquotes-app/src/App.js
+++ gotquotes-app/src/App.js
@@ -1,19 +1,61 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Route, Component } from "react";
+import axios from "axios";
+import QuoteList from "./components/QuoteList";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        QUOTES QUOTES QUOTES
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allAPIData: [],
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(
+        // "https://game-of-thrones-quotes.herokuapp.com/v1/random",
+        "https://game-of-thrones-quotes.herokuapp.com/v1/random/9",
+        // "https://game-of-thrones-quotes.herokuapp.com/v1/characters",
+        // "https://gameofthronesquotes.xyz/",
+
+        // "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=pizza/",
+        // "https://api.giphy.com/v1/gifs/search?api_key=Hrvht7esx5VuAneCKQ6apeDDwy5kD9ZT&q=${userInput}",
+        // "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=7K1lMY96eMDJ7GsheuvqeW4NwdKvRuHBFpiJY2eV&q=${https://www.nps.gov/afam/index.htm}",
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        // let quote = Object.keys(response.data[1].sentence);
+        // console.log("quote is", quote);
+
+        this.setState({
+          allAPIData: response.data,
+        });
+        console.log(this.state.allAPIData);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header allAPIData={this.state.allAPIData} />
+        <QuoteList allAPIData={this.state.allAPIData} />
+
+        {/* <Results allproducts={this.props.data} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// axios.post("https://api.infermedica.com/v2", data)
+//       .then(res => console.log(res))
+//       .catch(err => console.log(err));
+//   };
